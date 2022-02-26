c40d561adffb07147a8b832170f4ad9e43403445
jonrynn <jonrynn@gmail.com>
2022-02-26 14:38:14 -0500

    final

--- user.py
+++ user.py
@@ -1,68 +1,123 @@
 import sqlite3
+from turtle import update
 from flask_restful import Resource, reqparse
 
-class User:
-    def __init__(self, petID, petName, petOwner, petType):
-        self.petID = petID
-        self.petName = petName
-        self.petOwner = petOwner
-        self.petType = petType
 
-@classmethod
-def find_by_petid(cls, petID):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
 
-    query = "select * from users where petID=?"
-    result = cursor.execute(query, (petID,))
-    row = result.fetchone()
-    if row:
-        #user = cls(row[0],row[1], row[2], row[3])
-        user = cls(*row)
-    else:
-        user = None
 
-    connection.close()
-    return user 
 
 class User(Resource):
-    def get(self, petID):
-        user = next(filter(lambda x: x['petID'] == petID, users), None)
-        return {'user': user}, 200 if user else 404
+    TABLE_NAME = 'users'
 
-       
+    parser = reqparse.RequestParser()
+    parser.add_argument('petName')
+    parser.add_argument('petOwner' )
+    parser.add_argument('petType')
+
+    def get(self, petID):
+        user = self.find_by_petID(petID)
+        if user:
+            return user
+        return {'message': 'User not found'}, 404
+
+    @classmethod
+    def find_by_petID(cls, petID):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "select * from users where petID=?"
+        result = cursor.execute(query, (petID,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'petID': row[0], 'petName': row[1], 'petOwner': row[2], 'petType': row[3]}}
+
+
+    
 
     def post(self, petID):
-        if next(filter(lambda x: x['petID'] == petID, users), None):
-            return {'message': 'item already exists'}
-            
-        data = request.get_json()
-        user = {'petID': petID,
-                'petName': data['petName'], 
-                'petType': data['petType'],
-                'petOwner': data['petOwner'] }
-        users.append(user)
-        return user, 201
+        if self.find_by_petID(petID):
+            return {'message': "A user with petID '{}' already exists.".format(petID)}
 
-    def delete(self, petID):
-        global users
-        users = list(filter(lambda x: x['petID'] != petID, users))
-        return {'message': 'Item deleted'}
+        data = User.parser.parse_args()
+        
 
-    def put(self, petID):
-        data = request.get_json()
-        user = next(filter(lambda x: x['petID'] == petID, users), None)
-        if user is None:
-            user = {'petID': petID,
-                    'petName': data['petName'], 
-                    'petType': data['petType'],
-                    'petOwner': data['petOwner'] 
-            }
-            users.append(user)
-        else:
-            user.update(data)
+        user = {'petID': petID, 'petName': data['petName'], 'petOwner':data['petOwner'], 'petType': data['petType']}
+        
+        #try:
+        User.insert(user)
+        #except:
+        #    return {"message": "An error occurred inserting the user."}
+
         return user
 
+    @classmethod
+    def insert(cls, user):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES(?, ?,?,?)"
+        cursor.execute(query, (user['petID'], user['petName'], user['petOwner'], user['petType']))
+
+        connection.commit()
+        connection.close()
+
+
+    def delete(self, petID):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM users WHERE petID=?"
+        cursor.execute(query, (petID,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'user deleted'}
+
+
+    def put(self, petID):
+        data = User.parser.parse_args()
+        user = self.find_by_petID(petID)
+        updated_user = {'petID': petID, 'petName': data['petName'], 'petOwner':data['petOwner'], 'petType': data['petType']}
+  
+        if user is None:
+            try:
+                User.insert(updated_user)
+            except:
+                return {"message": "An error occurred inserting the user."}
+        else:
+            try:
+                User.update(updated_user)
+            except:
+                return {"message": "An error occurred updating the user."}
+        return updated_user
+
+    @classmethod
+    def update(cls, user):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE users SET petName=?, petOwner=?,petType=? WHERE petID=?"
+        cursor.execute(query, ( user['petName'], user['petOwner'], user['petType'],user['petID']))
+
+        connection.commit()
+        connection.close()
+
+
+
+
 class Users(Resource):
-    def get(self):
-        return users
+     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users"
+        result = cursor.execute(query)
+        users = []
+        for row in result:
+            users.append({'petID': row[0], 'petName': row[1], 'petOwner': row[2], 'petType': row[3]})
+        connection.close()
+#/Users/jonathanrynn/VSCode/cto_flask_venv/code/app.py
+        return {"users": users}
