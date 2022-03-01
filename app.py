92b9c5095996f2510336a335c09dc8a69a70ab8d
Jon Rynn <jonrynn@gmail.com>
2022-02-28 19:01:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
-import tkinter
+
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import  Api
 from user import User, Users
 
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app)
 api.add_resource(User, '/user/<string:petID>')
 api.add_resource(Users, '/users')
 
